0632d50582101b931f0232f3f69a5fbc4c653abd
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-06-15 01:27:39 +0300

    creat backend app

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express') // require the express package
+const app = express() // initialize your express app instance
+ 
+const cors = require('cors');
+
+app.use(cors()) // after you initialize your express app instance
+
+require('dotenv').config();
+
+const PORT=process.env.port;
+
+// a server endpoint 
+app.get('/', // our endpoint name
+ function (req, res) { // callback function of what we should do with our request
+  res.send('Hello World') // our endpoint function response
+})
+ 
+app.listen(8080) // kick start the express server to work
+
