540ce366ab01804363783476c35c5ad3890712ee
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-06-16 03:32:42 +0300

    edit

--- server.js
+++ server.js
@@ -1,38 +1,47 @@
-const express = require('express') // require the express package
-const app = express() // initialize your express app instance
+const express = require('express');// require the express package
+const app = express(); // initialize your express app instance
 const data = require('./data/weather.json');
+const axios = require('axios');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 const cors = require('cors');
 
-app.use(cors()) // after you initialize your express app instance
+app.use(cors()); // after you initialize your express app instance
 
-require('dotenv').config();
+// a server endpoint
 
-const PORT=process.env.PORT;
+app.get('/weather', (req, res) => {
 
-// a server endpoint 
-app.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-  res.send('Hello World') // our endpoint function response
-})
- 
-app.get('/weather', // our endpoint name
-  function (req, res) { // callback function of what we should do with our request
+  const lat = req.query.lat;
+  const lon = req.query.lon;
 
-    const resDataWeather = data.data.map( objDataWeather => new Weather(objDataWeather));
-    res.json(resDataWeather);// our endpoint function response
+  if (lat && lon) {
+    const reqWeatherBit = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
+
+    axios.get(reqWeatherBit).then(response => {
+      const responseDataWeatherBit = response.data.data.map(objWeatherBit => new Weather(objWeatherBit));
+      res.json(responseDataWeatherBit)
+    }).catch(error => {
+      res.send('Something went wrong :' . error.message)
+    });
+  } else {
+    res.send('You did not provide the lat and lon')
   }
-);
+
+
+});
+
 class Weather {
-    constructor(dataForWeather) {
-      this.descriptionWeather = dataForWeather.weather.description;
-      this.dateWeather = dataForWeather.valid_date;
-    }
+  constructor(dataForWeather) {
+    this.descriptionWeather = dataForWeather.weather.description;
+    this.dateWeather = dataForWeather.valid_date;
   }
-  
+}
 
 app.listen(PORT, () => {
-    console.log(`Server started on ${PORT}`);
-});
-// kick start the express server to work
+  console.log(`Server started : ${PORT}`);
+});k
 
